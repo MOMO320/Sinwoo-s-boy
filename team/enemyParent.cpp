d2a80e5e54e3160014243763c9c1d8e0b75215e8
preun <woghtjdah@naver.com>
2018-01-16 19:10:29 +0900

    실험

---
+++ team/enemyParent.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "enemyParent.h"
+
+
+enemyParent::enemyParent()
+{
+}
+
+
+enemyParent::~enemyParent()
+{
+}
---
+++ team/enemyParent.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "gameNode.h"
+class enemyParent :
+	public gameNode
+{
+public:
+	enemyParent();
+	~enemyParent();
+};
+
