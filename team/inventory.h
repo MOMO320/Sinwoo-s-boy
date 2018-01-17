736ca9e34c2f83e79326e5b023fbae5e719abd49
preun <woghtjdah@naver.com>
2018-01-17 17:36:31 +0900

    인벤토뢰 아이템 선택!

--- team/inventory.h
+++ team/inventory.h
@@ -16,6 +16,16 @@ class inventory :
 	itemParent* _boomerang;
 	itemParent* _potion;
 
+	//셀럭트 커서 깜빡임 위한 변수들
+	bool _cursor;
+	int _count;
+
+	float _x, _y;
+	//벡터에 가기 위한 인덱스 번호
+	int _index;
+	//초기설정을 위한 보이는 아이템 갯수
+	int _visibleItemNum;
+
 public:
 
 	HRESULT init();
