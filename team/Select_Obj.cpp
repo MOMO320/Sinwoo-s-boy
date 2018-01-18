5af61c739e4bba7ceb909edd31de1d5e896f2dc1
성위현 <songwehyun@hotmail.com>
2018-01-18 21:05:53 +0900

    커밋
    ㅋㅁ

--- team/Select_Obj.cpp
+++ team/Select_Obj.cpp
@@ -13,22 +13,38 @@ Select_Obj::~Select_Obj()
 
 HRESULT Select_Obj::init()	 
 {
+	SelectTile::init();
+	_vSampleTr = NULL;
+	_vSampleObj = NULL;
+	needFind = true;
+
+	TCHAR* items[] = { TEXT("오브젝트지형"),TEXT("STOP"),TEXT("PICK")};
+
+	_comboBox = CreateWindow("combobox", NULL, WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST, TOOLSIZEX - 500, 70, 155, 80, _hWnd, HMENU(BTN_COMBOBOX), _hInstance, NULL);
+
+	for (int i = 0; i < 3; i++)
+	{
+		SendMessage(_comboBox, CB_ADDSTRING, 0, (LPARAM)items[i]);
+	}
+
 	return S_OK;
 }
 
 void Select_Obj::release()	 
 {
+	SelectTile::release();
 }
 
 void Select_Obj::update()	 
 {
+	SelectTile::update();
 	switch (ComboBox_GetCurSel(_comboBox))
 	{
 	case 0:
 		if (needFind) {
 			if (_vSampleTr != NULL) SAFE_DELETE(_vSampleTr);
 
-			vTrInfo* _vSampleTr = TILEMANAGER->findTerrain_Index(TR_BASIC);
+			vObjInfo* _vSampleTr = TILEMANAGER->findObject_Index(OBJECT_NONE);
 			currentTileInfo = NULL;
 
 			sampleVectorClear();
@@ -36,8 +52,8 @@ void Select_Obj::update()
 			for (int i = 0; i < _vSampleTr->size(); i++)
 			{
 				lpSampleInfo temp = new sampleInfo;
-				temp->tileClass = TILE_TERRAIN;
-				temp->trInfo = (*_vSampleTr)[i];
+				temp->tileClass = TILE_OBJECT;
+				temp->objInfo = (*_vSampleTr)[i];
 				temp->rc = RectMake(TOOLSIZEX - 500 + (i % 5)*TILESIZE, 100 + (i / 5) * (TILESIZE + 5), TILESIZE, TILESIZE);
 				_vSampleTile.push_back(temp);
 			}
@@ -48,15 +64,34 @@ void Select_Obj::update()
 	case 1:
 		if (needFind) {
 			if (_vSampleTr != NULL) SAFE_DELETE(_vSampleTr);
-			vTrInfo* _vSampleTr = TILEMANAGER->findTerrain_Index(TR_CLIFF);
+			vObjInfo* _vSampleTr = TILEMANAGER->findObject_Index(OBJECT_STOP);
 			currentTileInfo = NULL;
 			sampleVectorClear();
 
 			for (int i = 0; i < _vSampleTr->size(); i++)
 			{
 				lpSampleInfo temp = new sampleInfo;
-				temp->tileClass = TILE_TERRAIN;
-				temp->trInfo = (*_vSampleTr)[i];
+				temp->tileClass = TILE_OBJECT;
+				temp->objInfo = (*_vSampleTr)[i];
+				temp->rc = RectMake(TOOLSIZEX - 500 + (i % 5)*TILESIZE, 100 + (i / 5) * (TILESIZE + 5), TILESIZE, TILESIZE);
+				_vSampleTile.push_back(temp);
+			}
+
+			needFind = false;
+		}
+		break;
+	case 2:
+		if (needFind) {
+			if (_vSampleTr != NULL) SAFE_DELETE(_vSampleTr);
+			vObjInfo* _vSampleTr = TILEMANAGER->findObject_Index(OBJECT_PICK);
+			currentTileInfo = NULL;
+			sampleVectorClear();
+
+			for (int i = 0; i < _vSampleTr->size(); i++)
+			{
+				lpSampleInfo temp = new sampleInfo;
+				temp->tileClass = TILE_OBJECT;
+				temp->objInfo = (*_vSampleTr)[i];
 				temp->rc = RectMake(TOOLSIZEX - 500 + (i % 5)*TILESIZE, 100 + (i / 5) * (TILESIZE + 5), TILESIZE, TILESIZE);
 				_vSampleTile.push_back(temp);
 			}
--- team/mainSwitch_ImageSet.cpp
+++ team/mainSwitch_ImageSet.cpp
@@ -8,7 +8,7 @@ void mainSwitch::imageSetting()
 	IMAGEMANAGER->addFrameImage("testObject", "./image./mapTile./test./object.bmp", 320, 64, 10, 2, false, RGB(255, 0, 255));
 	IMAGEMANAGER->addFrameImage("기본타일", "./image./mapTile./test./기본타일.bmp", 200, 200, 4, 4, false, RGB(255, 0, 255));
 	IMAGEMANAGER->addFrameImage("절벽", "./image./mapTile./test./절벽.bmp", 300, 600, 12, 24, false, RGB(255, 0, 255));
-
+	IMAGEMANAGER->addFrameImage("오브젝트타일", "./image./mapTile./objectTile./outTerriorObject.bmp", 200, 300, 4, 6, false, RGB(255, 0, 255));
 }
 
 void mainSwitch::tileSetting()
@@ -18,7 +18,7 @@ void mainSwitch::tileSetting()
 	POINT arr[5] = { {0,0},{2,2},{0,3},{0,2},{3,1} };
 	TILEMANAGER->addTerrain("기본지형", "기본타일", arr, 5, TR_BASIC);
 
-
-
+	//POINT arr2[1] = { { 0,3} };
+	TILEMANAGER->addObject("오브젝트타일", "오브젝트타일", { 0,2 }, { 3,3 }, { 0,0 }, OBJECT_PICK);
 
 }
