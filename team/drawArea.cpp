db1f73919233b0f1c6877d71e4a6df3811093235
신우 김 <sinsdhk@gmail.com>
2018-01-19 19:41:06 +0900

    g

--- team/drawArea.cpp
+++ team/drawArea.cpp
@@ -56,18 +56,6 @@ void drawArea::keyDownUpdate(int key)
 						{
 							for (int j = _tileX; j < _tileX + _SelectedTile->getSelectedTile()->objInfo->VOLUME.x; ++j)
 							{
-<<<<<<< HEAD
-								if(i< tileSizeX && j< tileSizeY)
-								(*_vCurrentTile)[j + i * tileSizeX]->setObject(*_SelectedTile->getSelectedTile()->objInfo);
-							}
-						}
-
-						//(*_vCurrentTile)[_tileY + _SelectedTile->getSelectedTile()->objInfo->VOLUME.y - 1 +
-						//	(_tileY + _SelectedTile->getSelectedTile()->objInfo->VOLUME.y - 1) * tileSizeX]->setObjectRender(true);
-
-					}
-
-=======
 								if (i < tileSizeY && j < tileSizeX)
 								{
 									(*_vCurrentTile)[j + i * tileSizeX]->setObject(*_SelectedTile->getSelectedTile()->objInfo);
@@ -77,7 +65,6 @@ void drawArea::keyDownUpdate(int key)
 							}
 						}
 					}
->>>>>>> 3a62f2af32c84c167670c6b10c7d6a54cd6bfb2c
 					break;
 				case TILE_EVENT:
 					break;
