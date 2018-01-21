bfb5629e4633bab08710024ea267710fab6b8072
신우 김 <sinsdhk@gmail.com>
2018-01-22 06:52:39 +0900

    맵툴~

--- team/tile.cpp
+++ team/tile.cpp
@@ -53,8 +53,26 @@ void tile::Toolrender(HDC hdc , int cameraX , int cameraY) //¸ÊºÎºÐ Ãâ·Â.
 	if (_object.OBJ_INDEX != OBJECT_NONE && _objectRender)
 	{
 		//_object._image->render(hdc, rc.left - cameraX, rc.top - cameraY, _object.imageIndex.x, _object.imageIndex.y, TILESIZE,TILESIZE);
-		Rectangle(hdc, rc.left - cameraX, rc.top - cameraY, rc.right - cameraX, rc.bottom - cameraY);
-		_object._image->render(hdc, rc.left - cameraX - TILESIZE * (_object.VOLUME.x-1) - _object._offSet.x, rc.top - cameraY - TILESIZE*(_object.VOLUME.y-1)-_object._offSet.y, _object.imageIndex.x, _object.imageIndex.y, _object.VOLUME.x *TILESIZE, _object.VOLUME.y * TILESIZE);
+		//Rectangle(hdc, rc.left - cameraX, rc.top - cameraY, rc.right - cameraX, rc.bottom - cameraY);
+		_object._image->render(hdc, rc.left - cameraX - TILESIZE * (_object.VOLUME.x-1) - _object._offSet.x, rc.top - cameraY - TILESIZE*(_object.VOLUME.y-1)-_object._offSet.y, _object.imageIndex.x, _object.imageIndex.y, _object.VOLUME.x *TILESIZE + _object._offSet.x, _object.VOLUME.y * TILESIZE + _object._offSet.y);
+		HBRUSH hb, hob;
+		hb = (HBRUSH)GetStockObject(NULL_BRUSH);
+		hob = (HBRUSH)SelectObject(hdc, hb);
+		HPEN hp, hop;
+		hp = (HPEN)CreatePen(PS_SOLID, 1, RGB(0, 0, 255));
+		hop = (HPEN)SelectObject(hdc, hp);
+		for (int i = 0; i < _object.VOLUME.y; i++)
+		{
+			for (int j = 0; j < _object.VOLUME.x; j++)
+			{
+				RectangleMake(hdc, rc.left - cameraX - j *TILESIZE, rc.top - i *TILESIZE - cameraY, TILESIZE,TILESIZE);
+			}
+		}
+		//RectangleMake(hdc, rc.left - cameraX - (_object.VOLUME.x - 1) *TILESIZE, rc.top - (_object.VOLUME.y - 1) *TILESIZE - cameraY, _object.VOLUME.x *TILESIZE, _object.VOLUME.y * TILESIZE);
+		SelectObject(hdc, hop);
+		SelectObject(hdc, hob);
+		DeleteObject(hp);
+		DeleteObject(hb);
 	}
 
 	char str[128];
