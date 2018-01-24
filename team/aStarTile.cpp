27181b37b30b92a91c8bea521c517ff263367fe4
박준홍 <keske89@naver.com>
2018-01-24 20:20:36 +0900

    A star 수정

--- team/aStarTile.cpp
+++ team/aStarTile.cpp
@@ -3,6 +3,8 @@
 
 
 aStarTile::aStarTile()
+	: _totalCost(0), _costFromStart(0),
+	_costToGoal(0), _parentNode(NULL), _idX(0), _idY(0)
 {
 }
 
@@ -14,13 +16,24 @@ aStarTile::~aStarTile()
 
 HRESULT aStarTile::init(int idX, int idY)
 {
+	_color = RGB(250, 150, 0);
+	_brush = CreateSolidBrush(_color);
+	_pen = CreatePen(PS_SOLID, 1, RGB(0, 0, 0));
 
+	_center = PointMake(idX * ASTARWIDTH + (ASTARWIDTH / 2),
+		idY * ASTARHEIGHT + (ASTARHEIGHT / 2));
+
+	_idX = idX;
+	_idY = idY;
+
+	_rc = RectMakeCenter(_center.x, _center.y, ASTARWIDTH, ASTARHEIGHT);
 
 	return S_OK;
 }
 void aStarTile::release()
 {
-
+	DeleteObject(_brush);
+	DeleteObject(_pen);
 }
 void aStarTile::update()
 {
@@ -28,5 +41,9 @@ void aStarTile::update()
 }
 void aStarTile::render()
 {
+	SelectObject(getMemDC(), (HBRUSH)_brush);
+	FillRect(getMemDC(), &_rc, _brush);
 
+	SelectObject(getMemDC(), (HPEN)_pen);
+	RectangleMake(getMemDC(), _rc.left, _rc.top, _rc.right, _rc.bottom);
 }
