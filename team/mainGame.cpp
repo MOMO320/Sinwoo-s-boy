f2595fc54dde94d9f62741989acce0f8e2dd2127
preun <woghtjdah@naver.com>
2018-01-31 16:45:08 +0900

    ㅁ

--- team/mainGame.cpp
+++ team/mainGame.cpp
@@ -29,7 +29,6 @@ HRESULT mainGame::init()			//초기화 함수
 
 	SCENEMANAGER->addScene("파일", new saveLoad);
 	SCENEMANAGER->addScene("입력창", new nameInput);
-	SCENEMANAGER->addScene("상점씬", new shopScene);
 	//SCENEMANAGER->addScene("타이틀", new title);
 	//SCENEMANAGER->addScene("인게임", new 인게임);
 	//SCENEMANAGER->changeScene("타이틀");
@@ -93,7 +92,7 @@ HRESULT mainGame::init()			//초기화 함수
 
 	//맵툴 로딩 테스트
 
-	//IGMAP->changeMap("castleB1");
+	IGMAP->changeMap("castleB1");
 
 	/*for (int i = 0; i < IGMAP->getCurrentPos()->size(); ++i)
 	{
@@ -107,7 +106,7 @@ HRESULT mainGame::init()			//초기화 함수
 	{
 		_em->setGreenSolider((*_map->getCurrentPos())[0]->vPatrol[i]);
 	}*/
-	IGMAP->changeMap("shop");
+	//IGMAP->changeMap("shop");
 		/*
 		CHARACTER_NONE,
 	CHARACTER_PLAYER_POS,
@@ -192,7 +191,7 @@ void mainGame::render()		//그려주는 함수(a.k.a WM_PAINT)
 	//==================== 건들지마라 ======================
 
 	//IMAGEMANAGER->findImage("카메라테스트배경")->render(getMemDC(), 0, 0, CAMERAMANAGER->getCameraPoint().x, CAMERAMANAGER->getCameraPoint().y, WINSIZEX, WINSIZEY);
-	IGMAP->render(getMemDC());
+	
 	char str[128];
 	sprintf(str, "메인게임페이지입니다.");
 	TextOut(getMemDC(), WINSIZEX / 2, WINSIZEY / 2, str, strlen(str));
@@ -205,29 +204,29 @@ void mainGame::render()		//그려주는 함수(a.k.a WM_PAINT)
 	}
 	else
 	{
-
+		IGMAP->render(getMemDC());
 		//아이템 사용시 날라가는 렌더
 		if (_player->getQuickItem() != NULL)
-		_player->getQuickItem()->render();
+			_player->getQuickItem()->render();
 
-	//_shop->render();
-	_em->render();
-	//_redEye->render();
-	_player->render();
+		//_shop->render();
+		_em->render();
+		//_redEye->render();
+		_player->render();
 
-	_om->render();
+		_om->render();
 
-	_redMoney->render();
-	_blueMoney->render();
-	_orangeMoney->render();
+		_redMoney->render();
+		_blueMoney->render();
+		_orangeMoney->render();
+
+		if (IGMAP->isCurrentMap("shop"))
+		{
+			_shop->render();
+		}
+
+		ASTARINFO->render(getMemDC());
 	}
-	if (IGMAP->isCurrentMap("shop"))
-	{
-		_shop->render();
-	}
-
-	ASTARINFO->render(getMemDC());
-	
 	//==================== 건들지마라 =======================
 	
 	this->getBackBuffer()->render(getHDC(), 0, 0);
